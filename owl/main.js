4abf4013cd2c992aa01ee8afee63b6eac7fdc608
syahshiimi <syahrulanuar@Syahruls-MacBook-Air.local>
2021-03-11 18:20:37 +0800

    background noise updates

--- owl/main.js
+++ owl/main.js
@@ -1,22 +1,8 @@
-$(document).ready(function () {
-	$(".owl-carousel").owlCarousel();
-});
-
 $(".owl-carousel").owlCarousel({
 	loop: true,
-	margin: 10,
+	items: 1,
+	margin: 30,
 	dots: false,
 	center: true,
-	autoHeight: true,
-	responsive: {
-		0: {
-			items: 1,
-		},
-		600: {
-			items: 1,
-		},
-		1000: {
-			items: 1
-		},
-	},
+	autoHeight: true
 })
